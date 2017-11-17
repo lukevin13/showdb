cd9fe84073272ba02204de78f34fd3b072e45394
Kevin Lu <lukevin13@gmail.com>
2017-11-17 11:39:25 -0500

    adds proptype validation

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HeroTitle from '../layouts/HeroTitle';
+import TMDB from '../helpers/TMDB';
 
 class ListPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      results: [],
     };
+
+    this.setResults = this.setResults.bind(this);
+    this.getShowList = this.getShowList.bind(this);
+
+    this.getShowList();
+  }
+
+  setResults(jsonResponse) {
+    const { results } = jsonResponse;
+    const validResults = results.filter(result => (result.backdrop_path && result.poster_path));
+
+    this.setState({
+      results: validResults,
+    });
+  }
+
+  getShowList() {
+    const { showType, listType, page } = this.props.match.params;
+    TMDB.getShowlist(showType, listType, this.setResults);
   }
 
   render() {
@@ -22,7 +42,19 @@ class ListPage extends React.Component {
 }
 
 ListPage.propTypes = {
-  match: PropTypes.shape({}).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      showType: PropTypes.string,
+      listType: PropTypes.string,
+      page: PropTypes.string,
+    }),
+  }),
+};
+
+ListPage.defaultProps = {
+  match: {
+    params: {},
+  },
 };
 
 export default ListPage;
