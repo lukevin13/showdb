59b967a10209edb21b8cbd91e232bc9e0f73ab41
Kevin Lu <lukevin13@gmail.com>
2017-11-17 16:23:12 -0500

    adds custom stylesheet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bulma/css/bulma.css';
+import './custom-styles.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
