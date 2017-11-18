0a6342f87dff5b0611411138dc87ac2cc2e08532
Kevin Lu <lukevin13@gmail.com>
2017-11-17 23:43:50 -0500

    adds more details to ShowDetails

--- src/components/ShowDetails.js
+++ src/components/ShowDetails.js
@@ -15,13 +15,44 @@ const ShowDetails = ({ show, clearModal }) => (
             </figure>
           </div>
           <div className="column is-8">
-            <p className="title">{show.title || show.name}</p>
+            <div className="box custom-box is-radiusless">
+              <p className="title">{show.title || show.name}</p>
 
+              <div className="columns is-mobile is-multiline">
+                <div className="column is-one-third-desktop is-one-third-tablet is-half-mobile">
+                  <p className="heading">Release Year</p>
+                  <p className="subtitle">{TMDB.getReleaseYear(show)}</p>
+                </div>
+                <div className="column is-one-third-desktop is-one-third-tablet is-half-mobile">
+                  <p className="heading">Runtime</p>
+                  <p className="subtitle">{(show.runtime) ? `${show.runtime} min` : 'N/A'}</p>
+                </div>
+                <div className="column is-one-third-desktop is-one-third-tablet is-half-mobile">
+                  <p className="heading">TMDB Score</p>
+                  <p className="subtitle">{show.vote_average || 'N/A'}</p>
+                </div>
+              </div>
 
+              <p className="heading">Genres</p>
+              <div className="tags">
+                {
+                  (show.genres.length > 0)
+                  ? (show.genres.map(genre => (
+                    <span className="tag is-danger is-medium" key={genre.id}>
+                      {genre.name}
+                    </span>
+                  )))
+                  : <span className="tag is-danger is-medium">N/A</span>
+                }
+              </div>
+
+              <p className="heading">Overview</p>
+              <p className="subtitle is-size-6">{show.overview || 'N/A'}</p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="modal-close is-large" onClick={clearModal} />
+      <button className="modal-close is-large" onClick={clearModal} />
     </div>
   </div>
 );
