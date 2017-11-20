da129bc9e6dc8ea2dedfb83e13e3f8259eac9f4d
Kevin Lu <lukevin13@gmail.com>
2017-11-20 18:27:50 -0500

    Update Footer.js

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
         <div className="buttons is-centered">
           <a className="button is-danger is-medium" href="https://reactjs.org/">React</a>
           <a className="button is-primary is-medium" href="https://bulma.io/">Bulma</a>
-          <a className="button is-link is-medium" href="https://lukevin13.github.io/showdb">Github</a>
+          <a className="button is-link is-medium" href="https://github.com/lukevin13/showdb">Github</a>
         </div>
         <p className="heading has-text-light">
           Movie and TV-Show information is retrieved from TMDB (The Movie Database) through their public API. Other than API access, this site has no affiliation with TMDB.
